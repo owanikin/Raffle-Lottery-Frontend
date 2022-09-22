479e0a50a985a97fce312153feef6d2d77829ff4
Owanikin <oderindeife@gmail.com>
2022-09-22 14:53:45 +0100

    useNotification

--- components/LotteryEntrance.js
+++ components/LotteryEntrance.js
@@ -3,14 +3,16 @@ import { abi, contractAddresses } from "../constants"
 import { useMoralis } from "react-moralis"
 import { useEffect, useState } from "react"
 import { ethers } from "ethers"
+import { useNotification } from "web3uikit"
 
 export default function LotteryEntrance() {
     const { chainId: chainIdHex, isWeb3Enabled } = useMoralis()
     const chainId = parseInt(chainIdHex)
-
     const raffleAddress = chainId in contractAddresses ? contractAddresses[chainId][0] : null
-
     const [entranceFee, setEntranceFee] = useState("0")
+
+    const dispatch = useNotification()
+
     const { runContractFunction: enterRaffle } = useWeb3Contract({
         abi: abi,
         contractAddress: raffleAddress,
@@ -36,6 +38,22 @@ export default function LotteryEntrance() {
             updateUI()
         }
     }, [isWeb3Enabled])
+
+    const handleSuccess = async function (tx) {
+        await tx.wait(1)
+        handleNewNotification(tx)
+    }
+
+    const handleNewNotification = function () {
+        dispatch({
+             type: "info",
+             message: "Transaction Complete!",
+             title: "Transaction Notification",
+             position: "topR",
+             icon: "",
+        })
+    }
+
     return (
         <div>
             Hi from Lottery Entrance!
@@ -43,7 +61,10 @@ export default function LotteryEntrance() {
                 <div>
                     <button
                         onClick={async function () {
-                            await enterRaffle()
+                            await enterRaffle({
+                                onSuccess: handleSuccess,
+                                onError: (error) => console.log(error),
+                            })
                         }}
                     >
                         Enter Raffle
